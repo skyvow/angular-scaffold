c879bfbcf3db0a29c9ae0a38aee459f4b3667077
skyvow <724355924@qq.com>
2017-03-09 17:22:12 +0800

    优化 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 var webpack            = require('webpack'),                                             // webpack
     path               = require('path'),                                                // 路径
     moment             = require('moment'),                                              // moment 
+    autoprefixer       = require("autoprefixer"),                                        // autoprefixer
     cssImport          = require("postcss-import"),                                      // import
     cssMixins          = require("postcss-mixins"),                                      // 函数
     cssExtend          = require("postcss-extend"),                                      // 继承
@@ -16,29 +17,28 @@ var webpack            = require('webpack'),                                    
     ExtractTextPlugin  = require("extract-text-webpack-plugin"),                         // css打包
     HtmlWebpackPlugin  = require('html-webpack-plugin'),                                 // 生成HTML文件
     CopyWebpackPlugin  = require('copy-webpack-plugin'),                                 // 文件copy
-    CleanWebpackPlugin = require('clean-webpack-plugin');                                // 清除文件
+    CleanWebpackPlugin = require('clean-webpack-plugin'),                                // 清除文件
+    OpenBrowserPlugin  = require('open-browser-webpack-plugin')                          // 自动打开浏览器
 
-
-// 配置基本路径
+// 配置路径
 var basePath = {
     src : 'src',
-    dist: 'build',
+    dist: 'build'
 }
 
 // 配置环境
 var ENV    = process.env.NODE_ENV,
-    isTest = ENV === 'development' || ENV === 'test',
-    isProd = ENV === 'production'  || ENV === 'build';
-
-console.log('process.env.NODE_ENV = ' + ENV)
+    isTest = ENV === 'development' || ENV === 'dev',
+    isProd = ENV === 'production'  || ENV === 'prod'
 
 // 配置参数
 var config = {
+    context: path.join(__dirname, ''),
 
-    // 页面入口文件配置
+    // 入口文件配置
     entry: {
         app: [
-            path.resolve(__dirname, basePath.src + '/modules/index.js')
+            path.resolve(__dirname, basePath.src, 'modules/index.js')
         ],
         vendor: [
             'moment',
@@ -55,9 +55,9 @@ var config = {
         ]
     },
 
-    // 入口文件输出配置
+    // 输出文件配置
     output: {
-        path         : basePath.dist,
+        path         : path.resolve(__dirname, basePath.dist),
         publicPath   : basePath.dist,
         filename     : '/assets/js/app.js',
         chunkFilename: '/chunk/[name].chunk.js'
@@ -73,7 +73,7 @@ var config = {
             dry: false
         }),
         
-        // 文件copy
+        // 复制文件
         new CopyWebpackPlugin([
             {
                 from: basePath.src + '/assets/',
@@ -81,13 +81,13 @@ var config = {
             }
         ]),
 
-        // 打包公共js文件
+        // 打包公共 js 文件
         new webpack.optimize.CommonsChunkPlugin('vendor', '/assets/js/vendor.js'),
 
         // moment
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn/),
 
-        // 将样式统一发布到app.css中
+        // 将样式统一发布到 app.css 中
         new ExtractTextPlugin('assets/css/app.css', {
             allChunks: true
         })
@@ -97,17 +97,17 @@ var config = {
     // 配置模块
     module: {
 
-        //加载器配置
+        // 加载器配置
         loaders: [
 
-            // ES6转码
+            // ES6 转码
             { 
                 test: /\.js$/, 
                 loader: 'babel', 
                 exclude: [path.resolve(__dirname, 'node_modules')], 
             },
 
-            // .css 文件使用postcss来处理并打包
+            // .css 文件使用 postcss 来处理并打包
             { 
                 test: /\.css$/, 
                 loader: ExtractTextPlugin.extract('style', 'css!postcss', {
@@ -118,14 +118,16 @@ var config = {
             // 内联 base64 URLs, 限定 <=8k 的图片, 其他的用 URL
             {
                 test: /\.(gif|jpg|png)\??.*$/,
-                loader: 'url-loader?limit=8192&name=assets/img/[hash].[ext]'
+                loader: 'url-loader?limit=8192&name=assets/images/[hash].[ext]'
             },
 
+            // 字体
             {
                 test: /\.(woff|svg|eot|ttf)\??.*$/,
                 loader: 'url-loader?limit=8192&name=assets/fonts/[hash].[ext]'
             },
 
+            // moment
             {
                 test: require.resolve('moment'),
                 loader: 'expose?moment'
@@ -151,6 +153,7 @@ var config = {
             cssSimpleVars(dependent),
             customMedia(dependent),
             cssAtroot(dependent),
+            autoprefixer(dependent)
         ]
 
         return processors
@@ -159,13 +162,13 @@ var config = {
     // 其它解决方案配置
     resolve: {
         alias: {
-            'ionic'        : path.resolve(__dirname, basePath.src + '/assets/plugins/ionic/js/ionic.js'),
-            'ionic-angular': path.resolve(__dirname, basePath.src + '/assets/plugins/ionic/js/ionic-angular.js'),
+            'ionic'        : path.resolve(__dirname, basePath.src, 'assets/plugins/ionic/js/ionic.js'),
+            'ionic-angular': path.resolve(__dirname, basePath.src, 'assets/plugins/ionic/js/ionic-angular.js')
         },
         extensions: ['', '.js', '.json', '.css'],
         root: [
             path.resolve(__dirname),
-            path.resolve(__dirname, 'src/'),
+            path.resolve(__dirname, basePath.src)
         ] 
     }
 }
@@ -182,8 +185,16 @@ if (isProd) {
     )
     config.entry.app.unshift(path.resolve(__dirname, 'prod.config.js'))
 } else {
-    config.devtool = 'source-map'
+    config.plugins.push(
+        // new OpenBrowserPlugin({ 
+        //     url: 'http://localhost:3000'
+        // }),
+        new webpack.DefinePlugin({
+            DEBUG: true
+        })
+    )
     config.entry.app.unshift(path.resolve(__dirname, 'dev.config.js'))
+    config.devtool = 'source-map'
 }
 
 module.exports = config
